ac48aaf8bc2056e80a72243538ef9c03164f88b5
patrick mugambi <patrick@kali.patrick>
2025-05-12 22:24:10 +0300

    create image upload form, model and view

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,16 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Image(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    image_file = models.ImageField(upload_to='images/')
+    submitter = models.ForeignKey(User, on_delete=models.CASCADE)
+    width = models.IntegerField(null=True, blank=True)
+    height = models.IntegerField(null=True, blank=True)
+    format = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from.forms import ImageUploadForm
 
-# Create your views here.
+#@login_required
+def upload_image(request):
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            image_instance = form.save(commit=False)
+            #image_instance.submitter = request.user
+            image_instance.save()
+            return redirect('core:index') # Redirect to the home page or another success page
+    else:
+        form = ImageUploadForm()
+    return render(request, 'gallery/upload.html', {'form': form})
