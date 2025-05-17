980be78ba36816385e33c8788dab06adb756236b
patrick mugambi <patrick@kali.patrick>
2025-05-17 17:39:04 +0300

    add likes field in Image model

--- gallery/models.py
+++ gallery/models.py
@@ -19,11 +19,13 @@ class Image(models.Model):
     height = models.IntegerField(null=True, blank=True)
     format = models.CharField(max_length=50, null=True, blank=True)
     tags = models.ManyToManyField(Tag)
+    likes = models.ManyToManyField(User, related_name='liked_images', blank=True) 
 
     def __str__(self):
         return self.title
-
-
+    
+    def total_likes(self):
+        return self.likes.count()
 
 
 class Collection(models.Model):
