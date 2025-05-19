dd2d5ddca48f75b98bea531eb3e761c649a927c7
patrick mugambi <patrick@kali.patrick>
2025-05-19 12:54:49 +0300

    create dockerfile & docker image

--- photos/settings.py
+++ photos/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-k0dfpp+&d=@^b_5bg&*&(lx7131_bhd#dciw*jnlcckuiwzvg7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
