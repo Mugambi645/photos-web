4053f236529c7daccfbfecc7cac20761c8dc2c58
patrick mugambi <patrick@kali.patrick>
2025-05-19 09:23:25 +0300

    create user profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
+from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    
+    path('user/<str:username>/', views.user_profile, name='user_profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from gallery.models import Image, Collection
 
-# Create your views here.
+def user_profile(request, username):
+    user = get_object_or_404(User, username=username)
+    uploaded_images = Image.objects.filter(submitter=user).order_by('-upload_date')
+    collections = Collection.objects.filter(owner=user).order_by('-name')
+
+    return render(request, 'accounts/user_profile.html', {
+        'profile_user': user,
+        'uploaded_images': uploaded_images,
+        'collections': collections,
+    })
